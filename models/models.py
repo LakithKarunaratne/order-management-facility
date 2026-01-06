ce07d63079a1ecf66554633ead8beff8692977c3
Lakith Karunaratne <lakith@asyncmetric.com>
2026-01-06 15:43:56 +0530

    relationship fix per spec

--- models/models.py
+++ models/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column, relationship
 # from db.config import Base
-from sqlalchemy import Integer, String, Column,DECIMAL, func
+from sqlalchemy import Integer, String, Column,DECIMAL, func, ForeignKey
 from datetime import datetime
 
 class Base(DeclarativeBase):
@@ -12,6 +12,7 @@ class Product(Base):
     name=Column(String(200))
     price = Column(DECIMAL(precision=19, scale=2))
     stock_qty=Column(Integer)
+    order_items = relationship("OrderItem", back_populates="product")
     
 
 class Order(Base):
@@ -19,12 +20,17 @@ class Order(Base):
     id = Column(Integer, primary_key=True, index=True)
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     status = Column(String(10))
+    order_items = relationship("OrderItem", back_populates="order")
 
 class OrderItem(Base):
     __tablename__ = "order_item"
     id = Column(Integer, primary_key=True, index=True)
-    prod_id = relationship("Product", back_populates="order_item")
+    order_id = Column(Integer, ForeignKey("order.id"))
+    prod_id = Column(Integer, ForeignKey("product.id"))
     qty_ordered = Column(Integer)
     price_at_time_of_order = Column(DECIMAL(precision=19, scale=2))
+    
+    order = relationship("Order", back_populates="order_items")
+    product = relationship("Product", back_populates="order_items")
     
 
